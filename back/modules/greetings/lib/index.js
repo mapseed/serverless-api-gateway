4c5344a699b1fc7a41bc15e625e53cf03bf1f641
Luke Swart <lukeswart@gmail.com>
2016-01-05 23:43:43 -0800

    Implement mapbox url reader and title creation

--- back/modules/greetings/lib/index.js
+++ back/modules/greetings/lib/index.js
@@ -1,6 +1,7 @@
 /**
  * Lib
  */
+var request = require('request')
 
 module.exports.respond = function(event, cb) {
 
@@ -8,5 +9,37 @@ module.exports.respond = function(event, cb) {
     message: "Your Serverless function ran successfully!"
   };
 
-  return cb(null, response);
+
+
+};
+
+
+module.exports.getMapboxWithTitle = function(event, cb) {
+  console.log("greetings/lib/index.js: event:", event);
+  request({
+    url: event.url,
+    method: 'GET',
+    json: true
+  }, function (error, response, body) {
+    if (error) {
+      console.log("error: ", error)
+      return { error: "error: " + error }
+    }
+    if (response.statusCode >= 400) {
+      console.log("status code >= 400: " + response.statusCode)
+      return { error: 'status code: ' + response.statusCode }
+    }
+    // Let's process the body to transform the feature.properties.title into feature.title:
+    var numberOfFeatures = body['features'].length;
+    console.log("typeof(body):", typeof(body));
+    console.log("numberOfFeatures:", numberOfFeatures);
+    var i;
+    for (i = 0; i < numberOfFeatures; i++) {
+      var featureTitle = body.features[i].properties.title;
+      var navTitle = featureTitle.replace(/<[^>]*>/g,'').replace(/[\/&:]/g, '').replace(/ /g, '-').replace(/--+/g, '-');
+      console.log("navTitle: ", navTitle);
+      body.features[i]['title'] = navTitle;
+    };
+    return cb(null, body);
+  })
 };
